a216d893d847feb0ec5368821ff1533d5499b32d
stevenjb5M <sb2568@byu.edu>
2025-11-11 14:21:19 -0700

    fixed client

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { fetchAuthSession } from 'aws-amplify/auth'
 
-const API_BASE_URL = 'http://localhost:5001'
+const API_BASE_URL = 'http://localhost:5000'
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
