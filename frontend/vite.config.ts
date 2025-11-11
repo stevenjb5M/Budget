5dfe22e52d1da6d38d6482d747b022c8646cf18c
stevenjb5M <sb2568@byu.edu>
2025-11-10 20:35:51 -0700

    hosted on aws

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public', // Ensure public folder is copied to dist
   server: {
     port: 5173,
     proxy: {
